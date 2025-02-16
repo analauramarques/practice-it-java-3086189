47c9c644e68bd6ca8af645c8337dc26c4edf2e6d
Ana Marques <92116593+analauramarques@users.noreply.github.com>
2025-02-16 03:12:27 +0000

    Employee.java _01_02b resolved

--- _01_02b/Employee.java
+++ _01_02b/Employee.java
@@ -3,37 +3,21 @@ package _01_02b;
 public class Employee {
 
   public static void main(String[] args) {
-
-    // Create a variable called age of type int and assign it the value 29.
-
-    // Print the age variable to the console.
-
-    // Create a variable called isAManager of type boolean and assign it the value
-    // true.
-
-    // Print the isAManager variable to the console.
-
-    // Create a variable called yearsOfService of type double and assign it the
-    // value 2.5.
-
-    // Print the yearsOfService variable to the console.
-
-    // Create a variable called baseSalary of type int and assign it the value 3000.
-
-    // Create a variable called overtimePayment of type int and assign it the value
-    // 40.
-
-    // Create a variable called totalPayment of type int and assign it to the value
-    // of baseSalary added to overtimePayment.
-
-    // Print the totalPayment variable to the console.
-
-    // Create three variables all of type double on a single line.
-    // They should be called firstBonus, secondBonus and thirdBonus and they should
-    // be assigned the values 10.00, 22.00 and 35.00.
-
-    // Print out the sum of the variables called firstBonus, secondBonus and
-    // thirdBonus.
+    int age = 29;
+    System.out.println(age);
+    boolean isAManager = true;
+    System.out.println(isAManager);
+    double yearsOfService = 2.5;
+    System.out.println(yearsOfService);
+    int baseSalary = 3000;
+    int overtimePayment = 40;
+    int totalPayment = (baseSalary + overtimePayment);
+    System.out.println(totalPayment);
+    double firstBonus, secondBonus, thirdBonus;
+    firstBonus = 10.00;
+    secondBonus = 22.00;
+    thirdBonus = 35.00;
+    System.out.println(firstBonus+secondBonus+thirdBonus);
 
   }
 
